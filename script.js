9945e48901f06fcd5e3c5d320c9f45c864d9550e
Yoshiki Aoki <yoshiki_aoki@dwango.co.jp>
2017-01-20 03:23:52 +0900

    Update bug fix

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-$(document).ready(() => {
-	const last_commit_title = document.querySelector('relative-time').title;
-	const last_commit_datetime = document.querySelector('relative-time').getAttribute('datetime');
-	const date = moment(last_commit_datetime);
-	const now = moment();
-	const diff_date = date.diff(now, 'months');
+function fresh() {
+  // if (document.querySelector('relative-time') === null) return;
+  // if (document.querySelector('relative-time').title === null) return;
+  // if (document.querySelector('.reponav-item.selected') !== document.getElementsByClassName('reponav-item')[0]) return;
+  const last_commit_title = document.querySelector('relative-time').title;
+  const last_commit_datetime = document.querySelector('relative-time').getAttribute('datetime');
+  const date = moment(last_commit_datetime);
+  const now = moment();
+  const diff_date = date.diff(now, 'months');
 
-	const insertElement = document.createElement('p');
-	insertElement.setAttribute('class', 'container');
-	insertElement.style.cssText = `
+  const insertElement = document.createElement('p');
+  insertElement.id = 'freshness';
+  insertElement.setAttribute('class', 'container');
+  insertElement.style.cssText = `
     background-color: #fafafa;
     margin-top: 15px;
     font-size: 15px;
@@ -15,14 +19,29 @@ $(document).ready(() => {
     margin-bottom: 15px;
   `
 
-	if (diff_date <= -12) {
-		insertElement.style.cssText += 'color: #FF2D2D';
-	} else if (diff_date <= -6) {
-		insertElement.style.cssText += 'color: #FDDC2B';
-	} else {
-		insertElement.style.cssText += 'color: #888';
-	}
+  if (diff_date <= -12) {
+    insertElement.style.cssText += 'color: #FF2D2D';
+  } else if (diff_date <= -6) {
+    insertElement.style.cssText += 'color: #FDDC2B';
+  } else {
+    insertElement.style.cssText += 'color: #888';
+  }
 
-	insertElement.innerHTML = 'Latest commit：' + last_commit_title;
-	$('.container.repohead-details-container').after(insertElement);
+  insertElement.innerHTML = 'Latest commit：' + last_commit_title;
+	const dom = document.getElementById('freshness');
+	if (dom === null) {
+  	$('.container.repohead-details-container').after(insertElement);
+	}
+}
+
+$(document).ready(() => {
+  window.addEventListener("click", () => {
+		console.log("ee");
+		fresh();
+	});
+  window.onpopstate = (e) => {
+    console.log(e.state);
+    fresh();
+  }
+  fresh();
 });
